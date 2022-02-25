be3eb339c065db25dc532f173e9f51b0e81eef57
unknown <azureuser@clientvm.azurebyngk.xyz>
2022-02-25 03:33:50 +0000

    added an nsg rule

--- gitdemo.tf
+++ gitdemo.tf
@@ -53,3 +53,24 @@ resource "azurerm_public_ip" "example" {
     environment = "Production"
   }
 }
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
