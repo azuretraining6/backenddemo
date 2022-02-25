c37a3c7d405b64b74ea76a6b9f8dd6690208af6c
azuretraining6 <98797951+azuretraining6@users.noreply.github.com>
2022-02-25 04:04:38 +0000

    Update gitdemo.tf

--- gitdemo.tf
+++ gitdemo.tf
@@ -1,9 +1,9 @@
 terraform {
      backend "azurerm" {
             resource_group_name  = "vmssdemo"
-    storage_account_name = "ngkcache45172"
+    storage_account_name = "ngkcache43345172"
     container_name       = "tfstate"
-    access_key                  = "ev5ouV/4s8ozCTmx4Tuj/SztiK+FYGV6RPCV47/O6TvP/P6gFBTYDl4/vOVcQqmpM8ln8hvWtU1uqmSQiba0Lg=="
+    access_key                  = "ev5ouV/4s8ozCTmx4Tuj/SztiK+FYGV6RPCV47/O6TvP/P6gasasaasasFBTYDl4/vOVcQqmpM8ln8hvWtU1uqmSQiba0Lg=="
     key = "terraform.tfstate"
       
     }
@@ -15,8 +15,8 @@ provider "azurerm" {
   features {}
 
 
-  subscription_id = "35a0c0cf-a662-4688-980d-d532b22f93c0"
-  client_id       = "950be8fb-2531-4aea-b44a-54a081213f46"
+  subscription_id = "xxxxxxxxxx"
+  client_id       = "950be8fb-2531-4aea-b4sss4a-54a081213f46"
   client_secret   = "g_77Q~TZ.jcXuHXR5lF2.awWMpdydxSOHhDGc"
   tenant_id       = "6dfb77b4-c9e1-4f45-ba05-02c1592c6232"
 }
